343c00cf31a19d6f571552cfa6d68514600fea82
Caleb Talbot <mailcaleb@gmail.com>
2016-08-03 17:01:10 -0500

    Initial Commit

---
+++ PhotoPhun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoPhun
+//
+//  Created by Caleb Talbot on 8/3/16.
+//  Copyright © 2016 Caleb Talbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoPhun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoPhun
+//
+//  Created by Caleb Talbot on 8/3/16.
+//  Copyright © 2016 Caleb Talbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
